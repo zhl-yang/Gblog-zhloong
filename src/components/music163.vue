8b438f486571d09d64df69c963362a04c99456bb
zhaolong.yang <zhaolong.yang@advantech.com.cn>
2022-07-15 11:18:38 +0800

    关闭自动播放

--- src/components/music163.vue
+++ src/components/music163.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <iframe class="music163" src="//music.163.com/outchain/player?type=0&id=78044620&auto=1&height=430" width="320"
+    <iframe class="music163" src="//music.163.com/outchain/player?type=0&id=78044620&auto=0&height=430" width="320"
             height="250"
     ></iframe>
     <iframe class="live2dHtml" src="//qcloudtest-1255546925.cos.ap-guangzhou.myqcloud.com/live2d.html" width="320"
@@ -8,7 +8,7 @@
   </div>
 </template>
 
-<style lang="less">
+<style scoped lang="less">
 
 .music163 {
   position: fixed;
